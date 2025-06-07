c79cc1765eb6426fd80f33fb990c7e0b3b7a8c7d
Adriana-Floress <adrianaflores.dn@gmail.com>
2025-06-06 19:47:13 -0600

    implementacion login

--- Front-End/src/Components/Login.jsx
+++ Front-End/src/Components/Login.jsx
@@ -92,7 +92,7 @@ const Login = () => {
                                 fontSize: "0.95rem"
                             }}
                         >
-                            Correo Electrónico
+                            Correo Electrónico 
                         </label>
                         <input 
                             type="email" 
